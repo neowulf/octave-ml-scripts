769ca4cbe8ca6cb2daee7e24b0764ceade2b8250
siva kommuri <snv.kommuri@gmail.com>
2013-11-08 23:51:55 -0800

    failed attempt at gradientDescent.m

--- mlclass-ex1/gradientDescent.m
+++ mlclass-ex1/gradientDescent.m
@@ -17,9 +17,37 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    
+    % alpha = 1e-5; % increasing to positive
+    %alpha = -1e-5; % going down but not converging >
+    %alpha = -6e-5; % went back up <
+    %alpha = -5e-5; % went back up < 6.77 up and slightly to the right
+    %alpha = -4e-5; % going down 6.43
+    %alpha = -1e-6; % going down 28.93
+    alpha = -3e-7; % went up too fast
+
+%== Connecting to ml-class ... iter: 1 cost: 148.14
+%iter: 2 cost: 148.14
+%iter: 3 cost: 148.13
+%iter: 4 cost: 148.12
+%iter: 5 cost: 148.11
+%iter: 6 cost: 148.10
+%iter: 7 cost: 148.09
+%iter: 8 cost: 148.08
+%iter: 9 cost: 148.07
+%iter: 10 cost: 148.06
+
+%== [ml-class] Submitted Assignment 1 - Part 3 - Gradi
+%== Sorry, your answer was incorrect.
 
 
 
+
+    cost = computeCost(X, y, theta);
+    temptheta = theta - (alpha * cost);
+    theta = temptheta;
+    disp(sprintf('iter: %d cost: %0.2f', iter, cost))
+    % disp(sprintf('iter: %d theta: %0.2f cost: %0.2f', iter, theta, cost))
 
 
 
